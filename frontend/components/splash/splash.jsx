28b2ec9b7137ddcecb8a08e08f2d2aad8e704923
Daniel <danielwu116@gmail.com>
2021-05-13 13:20:03 -0400

    Add new splash image

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import LoginFormContainer from '../session/login_form_container';
 
 const Splash = () => (
-  <div className='splash-div'>
-    <div>
-      <p className='splash-text'>Welcome to your professional links</p>
-      <LoginFormContainer/>
+  <>
+    <div className='splash-div'>
+      <div>
+        <p className='splash-text'>Welcome to your professional links</p>
+        <LoginFormContainer/>
+      </div>
+      <div>
+        <img src={window.splashImg} alt="splashImg" className='splashImg'/>
+      </div>
     </div>
-    <div>
-      <img src={window.splashImg} alt="splashImg" className='splashImg'/>
-    </div>
-  </div>
+  </>
 );
 
 export default Splash;
+
+// <p className='splash-creds'>Illustration by <a href="https://icons8.com/illustrations/author/5d99891e7dca3d0016cd4e34">Julia</a> from <a href="https://icons8.com/illustrations">Ouch!</a></p>
