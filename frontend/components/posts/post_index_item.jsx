52f11297a1e3e80da94a8269defc5d64d7c4547e
Daniel <danielwu116@gmail.com>
2021-05-14 00:32:14 -0400

    Hide dropdown button if post does not belong to currentUser

--- frontend/components/posts/post_index_item.jsx
+++ frontend/components/posts/post_index_item.jsx
@@ -18,7 +18,6 @@ class PostIndexItem extends React.Component {
   }
 
   clicked() {
-    debugger
     this.setState({drop: true});
   }
 
@@ -27,16 +26,28 @@ class PostIndexItem extends React.Component {
   }
   
   render() {
-    const { editPost, deletePost, users, post: { id, body, mediaUrl } } = this.props;
-    let postUser; let name;
+    const { deletePost, users, post: { id, body, mediaUrl } } = this.props;
+    let dropdown; let postUser; let name;
 
     if (this.state.user) {
       postUser = this.state.user;
       name = postUser.fname + ' ' + postUser.lname;
+
+      if (postUser.id == this.props.currentUser) {
+        dropdown = (
+          <button onFocus={this.clicked.bind(this)} onBlur={this.leave.bind(this)}>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Simple_icon_ellipsis.svg" alt="ellipsis"/>
+            <ul className={'post-dropdown ' + (this.state.drop ? 'reveal' : 'hide')}>
+              <li><i className="far fa-edit"></i>Edit Post</li>
+              <li><i className="far fa-trash-alt"></i>Delete Post</li>
+            </ul>
+          </button>
+        );
+      }
     } else {
       postUser = { headline: "" }
     }
-    
+    debugger
     return (
       <div className='post-item'>
         <header>
@@ -48,22 +59,16 @@ class PostIndexItem extends React.Component {
               <p>[Days ago posted]</p>
             </div>
           </div>
-          <button onFocus={this.clicked.bind(this)} onBlur={this.leave.bind(this)}>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Simple_icon_ellipsis.svg" alt="ellipsis"/>
-            <ul className={'post-dropdown ' + (this.state.drop ? 'reveal' : 'hide')}>
-              <li><i className="far fa-edit"></i>Edit Post</li>
-              <li><i className="far fa-trash-alt"></i>Delete Post</li>
-            </ul>
-          </button>
+          {dropdown}
         </header>
         <p>{body}</p>
-        <img src={mediaUrl} alt="" />
+        <img src={mediaUrl} alt=""/>
         <div className='num-lc'>
           [numLikes and numComments]
         </div>
         <div className='like-comment'>
-          <button><i class="far fa-thumbs-up"></i>Like</button>
-          <button><i class="far fa-comment-dots"></i>Comment</button>
+          <button><i className="far fa-thumbs-up"></i>Like</button>
+          <button><i className="far fa-comment-dots"></i>Comment</button>
         </div>
       </div>
     )
@@ -78,7 +83,7 @@ const mapSTP = ({ entities: { users }, session: { currentUser }}) => ({
 const mapDTP = dispatch => ({
   deletePost: postId => dispatch(deletePost(postId)),
   editPost: post => dispatch(editPost(post)),
-  fetchUser: userId => dispatch(fetchUser(userId))
+  fetchUser: userId => fetchUser(userId)
 });
 
 const PostIndexItemContainer = connect(mapSTP, mapDTP)(PostIndexItem)
