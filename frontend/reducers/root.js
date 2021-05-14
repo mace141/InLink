9724eb3961f51bb47d5e0caf60758271246b7916
Daniel <danielwu116@gmail.com>
2021-05-14 11:11:42 -0400

    Add modal actions & reducer. Add uiReducer

--- frontend/reducers/root.js
+++ frontend/reducers/root.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import entitiesReducer from './entities';
 import errorsReducer from './errors';
 import sessionReducer from './session';
+import uiReducer from './ui_reducer';
 
 const rootReducer = combineReducers({
   entities: entitiesReducer,
   session: sessionReducer,
-  errors: errorsReducer
+  errors: errorsReducer,
+  ui: uiReducer
 });
 
 export default rootReducer;
