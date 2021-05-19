1c439f1433c5cf10e6d1f25fea0b0e6535c10c95
Daniel <danielwu116@gmail.com>
2021-05-19 17:33:02 -0400

    Refactor user's reducer and API show view. Add receive connections & receive connection cases to users reducer

--- frontend/reducers/connection.js
+++ frontend/reducers/connection.js
@@ -1,12 +1,13 @@
-import { RECEIVE_CONNECTION, REMOVE_CONNECTION } from '../actions/connection';
+import { RECEIVE_CONNECTION, RECEIVE_CONNECTIONS, REMOVE_CONNECTION } from '../actions/connection';
 
 const connectionsReducer = (state = {}, action) => {
   Object.freeze(state);
 
   switch (action.type) {
+    case RECEIVE_CONNECTIONS:
+      return { ...state, ...action.connections };
     case RECEIVE_CONNECTION:
-      const { id } = action.connection;
-      return { ...state, [id]: action.connection };
+      return { ...state, ...action.connection };
     case REMOVE_CONNECTION:
       let nextState = { ...state };
       delete nextState[action.connectionId];
--- frontend/reducers/users.js
+++ frontend/reducers/users.js
@@ -1,16 +1,18 @@
+import { RECEIVE_CONNECTION, RECEIVE_CONNECTIONS } from '../actions/connection';
 import { RECEIVE_CURRENT_USER, RECEIVE_USER } from '../actions/session';
 
 const usersReducer = (state = {}, action) => {
   Object.freeze(state);
-  let id;
 
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
-      id = action.user.id;
-      return Object.assign({}, state, { [id]: action.user });
+      return { ...state, ...action.user };
     case RECEIVE_USER:
-      id = action.user.id;
-      return Object.assign({}, state, { [id]: action.user });
+      return { ...state, ...action.user };
+    case RECEIVE_CONNECTIONS:
+      return { ...state, ...action.users };
+    case RECEIVE_CONNECTION:
+      return { ...state, ...action.user };
     default:
       return state;
   }
