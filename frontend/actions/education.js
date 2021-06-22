4f0b2dab50b01b54c3c06ae59c96854aa2f6b13f
Daniel <danielwu116@gmail.com>
2021-06-22 14:26:58 -0400

    Fix post dropdown rendering

--- frontend/actions/education.js
+++ frontend/actions/education.js
@@ -4,11 +4,6 @@ export const RECEIVE_EDUCATIONS = 'RECEIVE_EDUCATIONS';
 export const RECEIVE_EDUCATION = 'RECEIVE_EDUCATION';
 export const REMOVE_EDUCATION = 'REMOVE_EDUCATION';
 
-const receiveEducations = educations => ({
-  type: RECEIVE_EDUCATIONS,
-  educations
-});
-
 const receiveEducation = education => ({
   type: RECEIVE_EDUCATION,
   education
@@ -18,18 +13,6 @@ const removeEducation = educationId => ({
   type: REMOVE_EDUCATION,
   educationId
 });
-
-export const fetchEducations = userId => dispatch => (
-  EducationAPI.fetchEducations(userId).then(
-    educations => dispatch(receiveEducations(educations))
-  )
-);
-
-export const fetchEducation = educationId => dispatch => (
-  EducationAPI.fetchEducation(educationId).then(
-    education => dispatch(receiveEducation(education))
-  )
-);
 
 export const createEducation = education => dispatch => (
   EducationAPI.createEducation(education).then(
--- frontend/actions/experience.js
+++ frontend/actions/experience.js
@@ -4,11 +4,6 @@ export const RECEIVE_EXPERIENCES = 'RECEIVE_EXPERIENCES';
 export const RECEIVE_EXPERIENCE = 'RECEIVE_EXPERIENCE';
 export const REMOVE_EXPERIENCE = 'REMOVE_EXPERIENCE';
 
-const receiveExperiences = experiences => ({
-  type: RECEIVE_EXPERIENCES,
-  experiences
-});
-
 const receiveExperience = experience => ({
   type: RECEIVE_EXPERIENCE,
   experience
@@ -18,18 +13,6 @@ const removeExperience = experienceId => ({
   type: REMOVE_EXPERIENCE,
   experienceId
 });
-
-export const fetchExperiences = userId => dispatch => (
-  ExperienceAPI.fetchExperiences(userId).then(
-    experiences => dispatch(receiveExperiences(experiences))
-  )
-);
-
-export const fetchExperience = experienceId => dispatch => (
-  ExperienceAPI.fetchExperience(experienceId).then(
-    experience => dispatch(receiveExperience(experience))
-  )
-);
 
 export const createExperience = experience => dispatch => (
   ExperienceAPI.createExperience(experience).then(
