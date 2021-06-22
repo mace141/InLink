1b1b74c916540a310251aa59156bb2a00ddcd86e
Daniel <danielwu116@gmail.com>
2021-06-22 11:18:20 -0400

    Add bottom padding to layout. Fix experience & education sorting

--- frontend/components/profile/profile.jsx
+++ frontend/components/profile/profile.jsx
@@ -65,19 +65,21 @@ class Profile extends React.Component {
 }
 
 const mapSTP = ({ entities: { users, educations, experiences } }, ownProps) => {
-  const mappedExp = Object.values(experiences).filter(
+  const sortedExp = Object.values(experiences).filter(
     exp => exp.userId == ownProps.match.params.id
   ).sort(
-    (a, b) => {Date.parse(a.endDate) > Date.parse(b.endDate) ? -1 : 1}
+    (a, b) => Date.parse(a.endDate) > Date.parse(b.endDate) ? -1 : (
+      Date.parse(a.startDate) > Date.parse(b.startDate) ? -1 : 1
+    )
   );
 
-  const mappedEdu = Object.values(educations).filter(
+  const sortedEdu = Object.values(educations).filter(
     edu => edu.userId == ownProps.match.params.id
-  ).sort((a, b) => a.endYear > b.endYear ? -1 : 1);
+  ).sort((a, b) => a.endYear > b.endYear ? -1 : a.startYear > b.startYear ? -1 : 1);
 
   return {
-    educations: mappedEdu,
-    experiences: mappedExp,
+    educations: sortedEdu,
+    experiences: sortedExp,
     user: users[ownProps.match.params.id]
   };
 };
