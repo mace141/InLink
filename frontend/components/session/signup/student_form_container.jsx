757971fc8244969b85a38af942507456d5675aa2
Daniel <danielwu116@gmail.com>
2021-09-13 16:01:11 -0400

    Change StudentForm to functional component

--- frontend/components/session/signup/student_form_container.jsx
+++ frontend/components/session/signup/student_form_container.jsx
@@ -1,68 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { createUser } from '../../../util/session_api';
 import { receiveCurrentUser, receiveUserStudent } from '../../../actions/session';
 import { createEducation } from '../../../actions/education';
 
-class StudentForm extends React.Component {
-  constructor(props) {
-    super(props);
-    const user = this.props.user;
+const StudentForm = ({ 
+  user, 
+  dispatch, 
+  receiveUserStudent, 
+  receiveCurrentUser, 
+  createEducation, 
+  createUser 
+}) => {
+  const [school, setSchool] = useState(user.school || '');
+  const [degree, setDegree] = useState(user.degree || '');
+  const [specialization, setSpecialization] = useState(user.specialization || '');
+  const [startYr, setStartYr] = useState(user.startYr || 2021);
+  const [endYr, setEndYr] = useState(user.endYr || 2031);
+  const [yearError, setYearError] = useState(false);
 
-    this.state = {
-      school: user.school || "",
-      degree: user.degree || "",
-      specialization: user.specialization || "",
-      startYr: user.startYr || 2021,
-      endYr: user.endYr || 2031,
-      yearErr: false
+  const handleInput = (field) => {
+    return e => {
+      const value = e.target.value;
+      switch (field) {
+        case 'school':
+          setSchool(value);
+          break;
+        case 'degree':
+          setDegree(value);
+          break;
+        case 'specialization':
+          setSpecialization(value);
+          break;
+        case 'startYr':
+          setStartYr(value);
+          break;
+        case 'endYr':
+          setEndYr(value);
+          break;
+        default: 
+          break;
+      }
     };
+  };
 
-    this.ensureForm = this.ensureForm.bind(this);
-  }
-
-  handleInput(field) {
-    return e => this.setState({ [field]: e.target.value });
-  }
-
-  ensureForm() {
-    const { school, degree, specialization } = this.state;
-
+  const ensureForm = () => {
     if (school.length && degree.length && specialization.length) {
       return false;
     } else {
       return true;
     }
-  }
+  };
 
-  handleErrors() {
-    const { startYr, endYr } = this.state;
+  const handleErrors = () => {
     let errorBool = false;
 
     if (parseInt(startYr) > parseInt(endYr)) {
-      this.setState({ yearErr: true });
+      setYearError(true);
       errorBool = true;
     }
 
     return errorBool;
-  }
+  };
 
-  handleSubmit(e) {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    this.setState({ yearErr: false });
+    setYearError(false);
 
-    if (!this.handleErrors()) {
+    if (!handleErrors()) {
       const student = {
-        headline: 'Student at ' + this.state.school,
-        industry: this.state.school,
-        ...this.state,
-        start_year: this.state.startYr,
-        end_year: this.state.endYr
+        headline: 'Student at ' + school,
+        industry: school,
+        school,
+        degree, 
+        specialization,
+        start_year: startYr,
+        end_year: endYr
       };
-      const { 
-        receiveUserStudent, createUser, createEducation, user, dispatch, receiveCurrentUser 
-      } = this.props;
       
       receiveUserStudent({ ...student });
       createUser({ ...user, ...student }).then(payload => {
@@ -73,56 +88,74 @@ class StudentForm extends React.Component {
         });
       });
     }
+  };
+
+  const years = [];
+  for (let i = 1962; i <= 2031; i++) {
+    years.unshift(i);
   }
 
-  render() {
-    const years = [];
-    for (let i = 1962; i <= 2031; i++) {
-      years.unshift(i);
-    }
-    const { yearErr } = this.state;
+  return (
+    <div className='signup-form'>
+      <h2>Your profile helps you discover new people and opportunities</h2>
+      <form onSubmit={handleSubmit} className='signup-form-white'>
+        <label>School or College/University *</label>
+        <input type="text" 
+               value={school} 
+               onChange={handleInput('school')}
+        />
 
-    return (
-      <div className='signup-form'>
-        <h2>Your profile helps you discover new people and opportunities</h2>
-        <form onSubmit={this.handleSubmit.bind(this)} className='signup-form-white'>
-          <label>School or College/University *</label>
-          <input type="text" value={this.state.school} onChange={this.handleInput('school')}/>
+        <label>Degree *</label>
+        <input type="text" 
+               value={degree} 
+               onChange={handleInput('degree')}
+        />
 
-          <label>Degree *</label>
-          <input type="text" value={this.state.degree} onChange={this.handleInput('degree')}/>
+        <label>Specialization *</label>
+        <input type="text" 
+               value={specialization} 
+               onChange={handleInput('specialization')}
+        />
 
-          <label>Specialization *</label>
-          <input type="text" value={this.state.specialization} onChange={this.handleInput('specialization')}/>
-
-          <div className='school-years-signup'>
-            <div className='school-year-signup' >
-              <label>Start year *</label>
-              <select className={'yr-selector-signup' + (yearErr ? ' input-error' : '')} onChange={this.handleInput('startYr')}>
-                {years.map(yr => {
-                  if (yr < 2022) return (<option key={yr} value={yr}>{yr}</option>)
-                })}
-              </select>
-            </div>
-
-            <div className='school-year-signup'>
-              <label>End year (or expected) *</label>
-              <select className={'yr-selector-signup' + (yearErr ? ' input-error' : '')} onChange={this.handleInput('endYr')}>
-                {years.map(yr => (
-                  <option key={yr} value={yr}>{yr}</option>
-                ))}
-              </select>
-            </div>
+        <div className='school-years-signup'>
+          <div className='school-year-signup' >
+            <label>Start year *</label>
+            <select 
+              className={'yr-selector-signup' + (yearError ? ' input-error' : '')} 
+              onChange={handleInput('startYr')}
+            >
+              {years.map(yr => {
+                if (yr < 2022) return (<option key={yr} value={yr}>{yr}</option>)
+              })}
+            </select>
           </div>
-          {yearErr ? <p className='error-msg'>Your end date can’t be earlier than your start date</p> : null }
 
-          <Link to='/signup/job' className='job-student'>I'm not a student</Link>
-          <button type='submit' className='form-button js-btn' disabled={this.ensureForm()}>Finish</button>
-        </form>
-      </div>
-    )
-  }
-}
+          <div className='school-year-signup'>
+            <label>End year (or expected) *</label>
+            <select 
+              className={'yr-selector-signup' + (yearError ? ' input-error' : '')} 
+              onChange={handleInput('endYr')}
+            >
+              {years.map(yr => (
+                <option key={yr} value={yr}>{yr}</option>
+              ))}
+            </select>
+          </div>
+        </div>
+        {yearError 
+          ? <p className='error-msg'>Your end date can’t be earlier than your start date</p> 
+          : null 
+        }
+
+        <Link to='/signup/job' className='job-student'>I'm not a student</Link>
+        <button type='submit' 
+                className='form-button js-btn' 
+                disabled={ensureForm()}
+        >Finish</button>
+      </form>
+    </div>
+  )
+};
 
 const mapSTP = ({ session: { signUp } }) => ({
   user: signUp
