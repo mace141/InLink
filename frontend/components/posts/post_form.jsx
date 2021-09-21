929af447547b800675e2460134d1ca120d084440
Daniel <danielwu116@gmail.com>
2021-09-21 19:27:25 -0400

    Change PostForm to functional component

--- frontend/components/posts/post_form.jsx
+++ frontend/components/posts/post_form.jsx
@@ -1,62 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-class PostForm extends React.Component {
-  constructor(props) {
-    super(props);
+const PostForm = ({ post, user, name, formType, processForm, closeModal }) => {
+  const [body, setBody] = useState(post.body);
+  const [media, setMedia] = useState(post.media);
+  const [mediaUrl, setMediaUrl] = useState(post.mediaUrl);
 
-    this.state = this.props.post;
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleInput = this.handleInput.bind(this);
-    this.handleFile = this.handleFile.bind(this);
-    this.ensureContent = this.ensureContent.bind(this);
-    this.ensureMedia = this.ensureMedia.bind(this);
-    this.openFileLoader = this.openFileLoader.bind(this);
-    this.removeFile = this.removeFile.bind(this);
-    this.closePostForm = this.closePostForm.bind(this);
-  }
-
-  componentDidMount() {
-    if (this.state.mediaUrl) {
+  useEffect(() => {
+    if (mediaUrl) {
       const postBodies = document.getElementsByClassName('post-body')
 
       for (let postBody of postBodies) {
         postBody.classList.add('overflow');
       }
     }
-  }
+  }, []);
 
-  ensureContent() {
-    const { body, media } = this.state;
-
+  const ensureContent = () => {
     if (body.length || media) {
       return false;
     } else {
       return true;
     }
-  }
+  };
 
-  ensureMedia() {
-    if (this.state.media) return false;
+  const ensureMedia = () => {
+    if (media) return false;
     return true;
-  }
+  };
 
-  handleInput(e) {
-    this.setState({ body: e.target.value });
-  }
+  const handleInput = (e) => {
+    setBody(e.target.value);
+  };
 
-  openFileLoader() {
+  const openFileLoader = () => {
     document.getElementById('post-media-input').click();
-    this.mediaModalSwitch();
-  }
+    mediaModalSwitch();
+  };
 
-  handleFile(e) {
+  const handleFile = (e) => {
     const file = e.target.files[0];
     
     const fileReader = new FileReader();
     // will run when file is read
     fileReader.onloadend = () => {
-      this.setState({ media: file, mediaUrl: fileReader.result });
+      setMedia(file);
+      setMediaUrl(fileReader.result);
       const postBodies = document.getElementsByClassName('post-body');
 
       for (let postBody of postBodies) {
@@ -68,13 +56,11 @@ class PostForm extends React.Component {
       // reads the file
       fileReader.readAsDataURL(file);
     }
-  }
+  };
 
-  removeFile() {
-    this.setState({
-      media: null,
-      mediaUrl: null
-    });
+  const removeFile = () => {
+    setMedia(null);
+    setMediaUrl(null);
     document.getElementById('post-media-input').value = null;
 
     const postBodies = document.getElementsByClassName('post-body');
@@ -82,138 +68,131 @@ class PostForm extends React.Component {
     for (let postBody of postBodies) {
       postBody.classList.remove('overflow');
     }
-  }
+  };
 
-  closePostForm() {
-    const { body, mediaUrl } = this.state;
-    const { post, closeModal } = this.props; 
-
+  const closePostForm = () => {
     if (body != post.body || mediaUrl != post.mediaUrl) {
-      this.discardModalSwitch();
+      discardModalSwitch();
     } else {
       closeModal();
     }
-  }
+  };
 
-  discardModalSwitch() {
+  const discardModalSwitch = () => {
     document.getElementsByClassName('post-form-modal')[0].classList.toggle('hidden');
     document.getElementsByClassName('post-discard-modal')[0].classList.toggle('hidden');
-  }
+  };
 
-  mediaModalSwitch() {
+  const mediaModalSwitch = () => {
     document.getElementsByClassName('post-form-modal')[0].classList.toggle('hidden');
     document.getElementsByClassName('post-media-modal')[0].classList.toggle('hidden');
-  }
+  };
 
-  handleSubmit(e) {
+  const handleSubmit = (e) => {
     e.preventDefault();
     
     // creates an object to send to controller
     const formData = new FormData();
-    if (this.state.media) {
-      formData.append('post[media]', this.state.media);
+    if (media) {
+      formData.append('post[media]', media);
     }
-    if (this.state.id) {
-      formData.append('post[id]', this.state.id);
+    if (post.id) {
+      formData.append('post[id]', post.id);
     }
-    formData.append('post[body]', this.state.body);
-    formData.append('post[user_id]', this.state.userId);
+    formData.append('post[body]', body);
+    formData.append('post[user_id]', post.userId);
     
-    this.props.processForm(formData);
-    this.setState({ 
-      body: "",
-      media: null,
-      mediaUrl: null
-    });
+    processForm(formData);
+    setBody('');
+    setMedia(null);
+    setMediaUrl(null);
     document.getElementById('post-media-input').value = "";
-    this.props.closeModal();
-  }
+    closeModal();
+  };
 
-  render() {
-    // image preview
-    const preview = (noPic) => this.state.mediaUrl ? <img src={this.state.mediaUrl}/> : noPic;
-    const selectMedia = (
-      <span onClick={() => document.getElementById('post-media-input').click()}>Select images to share</span>
-    );
-    const closeImageBtn = (
-      this.state.media ? <span className='remove-img-btn' onClick={this.removeFile}>✕</span> : null
-    );
-    
-    return (
-      <>
-        <div className='modal post-form-modal'>
-          <header>
-            <h2>{this.props.formType}</h2>
-            <span className='close-modal-button' 
-                  onClick={this.closePostForm}
-            >✕</span>
-          </header>
-          <form onSubmit={this.handleSubmit} className='post-form'>
-            <div className='post-body og'>
-              <div>
-                <div className='avatar small'>
-                  <img src={this.props.user.avatarUrl || window.defaultUser} alt="Profile Pic" className='pfp small'/>
-                </div>
-                <h2>{this.props.name}</h2>
-              </div>
-              <div className='textarea'>
-                <textarea placeholder='What do you want to talk about?' value={this.state.body} onChange={this.handleInput}></textarea>
-              </div>
-              <div className='image-body'>
-                {closeImageBtn}
-                {preview(null)}
-              </div>
-            </div>
-            <footer className='post-form-footer footer'>
-              <i className="far fa-image" onClick={this.openFileLoader}></i>
-              <input type="file" id='post-media-input' accept='image/*' onChange={this.handleFile}/>
-              <button className='form-button' disabled={this.ensureContent()}>Post</button>
-            </footer>
-          </form>
-        </div>
-        <div className='modal post-media-modal hidden'>
-          <header>
-            <h2>Edit your photo</h2>
-            <span className='close-modal-button' onClick={this.mediaModalSwitch}>✕</span>
-          </header>
-          <div className='post-body image-body'>
-            {preview(selectMedia)}
-          </div>
-          <footer>
+  // image preview
+  const preview = (noPic) => mediaUrl ? <img src={mediaUrl}/> : noPic;
+  const selectMedia = (
+    <span onClick={() => document.getElementById('post-media-input').click()}>Select images to share</span>
+  );
+  const closeImageBtn = (
+    media ? <span className='remove-img-btn' onClick={removeFile}>✕</span> : null
+  );
+  
+  return (
+    <>
+      <div className='modal post-form-modal'>
+        <header>
+          <h2>{formType}</h2>
+          <span className='close-modal-button' 
+                onClick={closePostForm}
+          >✕</span>
+        </header>
+        <form onSubmit={handleSubmit} className='post-form'>
+          <div className='post-body og'>
             <div>
-              <button className='back-btn'
-                      onClick={() => { this.removeFile(); this.mediaModalSwitch() }}
-              >Back</button>
-              <button className='done-btn' disabled={this.ensureMedia()} 
-                      onClick={this.mediaModalSwitch}
-              >Done</button>
+              <div className='avatar small'>
+                <img src={user.avatarUrl || window.defaultUser} alt="Profile Pic" className='pfp small'/>
+              </div>
+              <h2>{name}</h2>
             </div>
-          </footer>
-        </div>
-        <div className='modal post-discard-modal hidden'>
-          <header>
-            <h2>Discard {this.props.formType === 'Edit post' ? 'changes': 'draft'}</h2>
-            <span className='close-modal-button' onClick={this.discardModalSwitch}>✕</span>
-          </header>
-          <div className='post-body discard'>
-            <span>
-              Are you sure you want to discard your {this.props.formType === 'Edit post' ? 'changes': 'draft'}?
-            </span>
+            <div className='textarea'>
+              <textarea placeholder='What do you want to talk about?' value={body} onChange={handleInput}></textarea>
+            </div>
+            <div className='image-body'>
+              {closeImageBtn}
+              {preview(null)}
+            </div>
           </div>
-          <footer>
-            <div>
-              <button className='back-btn' onClick={this.discardModalSwitch}>
-                Back
-              </button>
-              <button className='done-btn' onClick={() => this.props.closeModal()}>
-                Discard
-              </button>
-            </div>
+          <footer className='post-form-footer footer'>
+            <i className="far fa-image" onClick={openFileLoader}></i>
+            <input type="file" id='post-media-input' accept='image/*' onChange={handleFile}/>
+            <button className='form-button' disabled={ensureContent()}>Post</button>
           </footer>
+        </form>
+      </div>
+      <div className='modal post-media-modal hidden'>
+        <header>
+          <h2>Edit your photo</h2>
+          <span className='close-modal-button' onClick={mediaModalSwitch}>✕</span>
+        </header>
+        <div className='post-body image-body'>
+          {preview(selectMedia)}
         </div>
-      </>
-    )
-  }
+        <footer>
+          <div>
+            <button className='back-btn'
+                    onClick={() => { removeFile(); mediaModalSwitch() }}
+            >Back</button>
+            <button className='done-btn' disabled={ensureMedia()} 
+                    onClick={mediaModalSwitch}
+            >Done</button>
+          </div>
+        </footer>
+      </div>
+      <div className='modal post-discard-modal hidden'>
+        <header>
+          <h2>Discard {formType === 'Edit post' ? 'changes': 'draft'}</h2>
+          <span className='close-modal-button' onClick={discardModalSwitch}>✕</span>
+        </header>
+        <div className='post-body discard'>
+          <span>
+            Are you sure you want to discard your {formType === 'Edit post' ? 'changes': 'draft'}?
+          </span>
+        </div>
+        <footer>
+          <div>
+            <button className='back-btn' onClick={discardModalSwitch}>
+              Back
+            </button>
+            <button className='done-btn' onClick={() => closeModal()}>
+              Discard
+            </button>
+          </div>
+        </footer>
+      </div>
+    </>
+  )
 };
 
 export default PostForm;
