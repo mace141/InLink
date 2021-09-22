82bb6c4bc7c7606c7781e28226c8630b203b9f36
Daniel <danielwu116@gmail.com>
2021-09-21 22:23:12 -0400

    Change Main to functional component

--- frontend/components/main/main.jsx
+++ frontend/components/main/main.jsx
@@ -4,21 +4,14 @@ import FeedContainer from '../feed/feed';
 import NetworkContainer from '../network/network';
 import ProfileContainer from '../profile/profile'
 
-class Main extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <>
-        <ProtectedRoute exact path='/feed' component={FeedContainer}/>
-        <ProtectedRoute exact path='/users/:id' component={ProfileContainer}/>
-        <ProtectedRoute exact path='/mynetwork' component={NetworkContainer}/>
-      </>
-    )
-  }
-}
-
+const Main = () => {
+  return (
+    <>
+      <ProtectedRoute exact path='/feed' component={FeedContainer}/>
+      <ProtectedRoute exact path='/users/:id' component={ProfileContainer}/>
+      <ProtectedRoute exact path='/mynetwork' component={NetworkContainer}/>
+    </>
+  );
+};
 
 export default Main;
