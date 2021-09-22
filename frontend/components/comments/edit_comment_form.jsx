fa1a09aef9924e97a826e2faa65e531a4a717269
Daniel <danielwu116@gmail.com>
2021-09-22 11:59:46 -0400

    Change EditCommentForm to functional component

--- frontend/components/comments/edit_comment_form.jsx
+++ frontend/components/comments/edit_comment_form.jsx
@@ -2,63 +2,62 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { updateComment } from '../../actions/comment';
 
-class EditCommentForm extends React.Component {
-  constructor(props) {
-    super(props);
+const EditCommentForm = ({
+  comment,
+  comment: { body },
+  updateComment,
+  cancelEdit
+}) => {
+  const [formState, setFormState] = useState(comment);
+  const originalComment = body;
 
-    this.state = this.props.comment;
-    this.originalComment = this.props.comment.body;
-    
-    this.handleInput = this.handleInput.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.ensureChange = this.ensureChange.bind(this);
-  }
+  const handleInput = (e) => {
+    setFormState({ ...formState, body: e.target.value });
+  };
 
-  handleInput(e) {
-    this.setState({ body: e.target.value });
-  }
-
-  handleSubmit() {
+  const handleSubmit = () => {
+    const { media, parentCommentId, id, body, userId } = formState;
     const formData = new FormData();
-    if (this.state.media) {
-      formData.append('comment[media]', this.state.media);
+
+    if (media) {
+      formData.append('comment[media]', media);
     }
-    if (this.state.parentCommentId) {
-      formData.append('comment[parent_comment_id]', this.state.parentCommentId);
+    if (parentCommentId) {
+      formData.append('comment[parent_comment_id]', parentCommentId);
     }
-    formData.append('comment[id]', this.state.id);
-    formData.append('comment[body]', this.state.body);
-    formData.append('comment[user_id]', this.state.userId);
+    formData.append('comment[id]', id);
+    formData.append('comment[body]', body);
+    formData.append('comment[user_id]', userId);
     
-    this.props.updateComment(formData);
-    this.setState({ 
+    updateComment(formData);
+    setFormState({ 
+      ...formState,
       body: "",
       media: null,
       mediaUrl: null
     });
-    this.props.cancelEdit();
-  }
+    cancelEdit();
+  };
 
-  ensureChange() {
-    if (this.state.body === this.originalComment) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+  const ensureChange = () => {
+    return formState.body === originalComment;
+  };
 
-  render() {
-    return (
-      <div className='gray-shade edit'>
-        <input type="text" value={this.state.body} onChange={this.handleInput}/>
-        <p className='edit-cmt-btns'>
-          <button className='save-cmt-edit' onClick={this.handleSubmit} disabled={this.ensureChange()}>Save Changes</button>
-          <button className='cancel-cmt-edit' onClick={this.props.cancelEdit}>Cancel</button>
-        </p>
-      </div>
-    )
-  }
-}
+  return (
+    <div className='gray-shade edit'>
+      <input type="text" value={formState.body} onChange={handleInput}/>
+      <p className='edit-cmt-btns'>
+        <button className='save-cmt-edit' 
+                onClick={handleSubmit} 
+                disabled={ensureChange()}
+        >Save Changes</button>
+        <button className='cancel-cmt-edit' 
+                onClick={cancelEdit}
+        >Cancel</button>
+      </p>
+    </div>
+  );
+};
 
 const mapDTP = dispatch => ({
   updateComment: comment => dispatch(updateComment(comment))
