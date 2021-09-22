abaf444d266079a9bca7f1539c588984210ce2ad
Daniel <danielwu116@gmail.com>
2021-09-22 10:38:13 -0400

    Change ExperienceIndex to functional component

--- frontend/components/experiences/experience_index.jsx
+++ frontend/components/experiences/experience_index.jsx
@@ -4,30 +4,32 @@ import { withRouter } from 'react-router-dom';
 import { openModal } from '../../actions/modal';
 import ExperienceIndexItemContainer from './experience_index_item';
 
-class ExperienceIndex extends React.Component {
-  render() {
-    const { currentUser, match, openModal, experiences } = this.props;
-    const newExpBtn = String(currentUser) === match.params.id ? (
-      <button onClick={() => openModal('createExp')} className='open-exp'>
-        <i className="fas fa-plus"></i>
-      </button>
-    ) : null;
+const ExperienceIndex = ({
+  currentUser,
+  experiences,
+  match,
+  openModal
+}) => {
+  const newExpBtn = String(currentUser) === match.params.id 
+    ? (<button onClick={() => openModal('createExp')} className='open-exp'>
+         <i className="fas fa-plus"></i>
+       </button>) 
+    : null;
 
-    if (!experiences.length) return null;
+  if (!experiences.length) return null;
 
-    return (
-      <div className='exp-index'>
-        <h1>Experience</h1>
-        {newExpBtn}
-        <ul>
-          {experiences.map(exp => (
-            <ExperienceIndexItemContainer key={exp.id} experience={exp}/>
-            ))}
-        </ul>
-      </div>
-    );
-  }
-}
+  return (
+    <div className='exp-index'>
+      <h1>Experience</h1>
+      {newExpBtn}
+      <ul>
+        {experiences.map(exp => (
+          <ExperienceIndexItemContainer key={exp.id} experience={exp}/>
+          ))}
+      </ul>
+    </div>
+  );
+};
 
 const mapSTP = ({ session: { currentUser } }) =>({
   currentUser
