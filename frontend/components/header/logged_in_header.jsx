ae986917e5641475511c69bce28c513082cf0e23
Daniel <danielwu116@gmail.com>
2021-09-21 22:26:13 -0400

    Change LoggedIn to functional component

--- frontend/components/header/logged_in_header.jsx
+++ frontend/components/header/logged_in_header.jsx
@@ -1,87 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { Link, withRouter } from 'react-router-dom';
 import { logoutUser } from '../../actions/session';
 import SearchBar from './search_bar';
 
-class LoggedIn extends React.Component {
-  constructor(props) {
-    super(props);
+const LoggedIn = ({
+  user,
+  history,
+  logoutUser
+}) => {
+  const [drop, setDrop] = useState(false);
 
-    this.state = { 
-      drop: false,
-    };
+  const clicked = () => {
+    setDrop(true);
+  };
 
-    this.clicked = this.clicked.bind(this);
-    this.leave = this.leave.bind(this);
-  }
+  const leave = () => {
+    setDrop(false);
+  };
 
-  clicked() {
-    this.setState({ drop: true });
-  }
+  const profile = user.avatarUrl || window.defaultUser;
 
-  leave() {
-    this.setState({ drop: false });
-  }
-
-  render() {
-    const { user, logoutUser } = this.props;
-    const profile = user.avatarUrl || window.defaultUser;
-
-    return (
-      <>
-        <nav className='left-nav-bar'>
-          <Link to='/'>
-            <div className='icon-logo'>
-              <img src={window.iconLogo} alt="InLink-icon-logo" className='InLink-icon-logo'/>
+  return (
+    <>
+      <nav className='left-nav-bar'>
+        <Link to='/'>
+          <div className='icon-logo'>
+            <img src={window.iconLogo} alt="InLink-icon-logo" className='InLink-icon-logo'/>
+          </div>
+        </Link>
+        <SearchBar/>
+      </nav>
+      <nav className='right-nav-bar'>
+        <Link to='/feed'>
+          <div className='nav-icon'>
+            <i className="fas fa-home"></i>
+            <p>Home</p>
+          </div>
+        </Link>
+        <Link to='/mynetwork'>
+          <div className='nav-icon'>
+            <i className="fas fa-user-friends"></i>
+            <p>My Network</p>
+          </div>
+        </Link>
+        <button onFocus={clicked} onBlur={leave} className='user-sesh-btn'>
+          <div className='header-avatar'>
+            <div className='avatar'>
+              <img src={profile} alt="Profile Pic" className='pfp small'/>
             </div>
-          </Link>
-          <SearchBar/>
-        </nav>
-        <nav className='right-nav-bar'>
-          <Link to='/feed'>
-            <div className='nav-icon'>
-              <i className="fas fa-home"></i>
-              <p>Home</p>
-            </div>
-          </Link>
-          <Link to='/mynetwork'>
-            <div className='nav-icon'>
-              <i className="fas fa-user-friends"></i>
-              <p>My Network</p>
-            </div>
-          </Link>
-          <button onFocus={this.clicked} onBlur={this.leave} className='user-sesh-btn'>
-            <div className='header-avatar'>
-              <div className='avatar'>
-                <img src={profile} alt="Profile Pic" className='pfp small'/>
-              </div>
-              <p>Me <span className='arrow-down'></span></p>
-            </div>
-            <ul className={'header-dropdown ' + (this.state.drop ? 'reveal' : 'hide')}>
-              <li>
-                <div>
-                  <div className='avatar'>
-                    <img src={profile} alt="Profile Pic" className='pfp'/>
-                  </div>
-                  <div>
-                    <p>{`${user.fname} ${user.lname}`}</p>
-                    <p>{user.headline}</p>
-                  </div>
+            <p>Me <span className='arrow-down'></span></p>
+          </div>
+          <ul className={'header-dropdown ' + (drop ? 'reveal' : 'hide')}>
+            <li>
+              <div>
+                <div className='avatar'>
+                  <img src={profile} alt="Profile Pic" className='pfp'/>
                 </div>
-                <div className='pf-btn' onClick={() => {
-                  this.props.history.push(`/users/${user.id}`);
-                  this.leave();
-                }}>View Profile</div>
-              </li>
-              <li onClick={logoutUser}>Sign Out</li>
-            </ul>
-          </button>
-        </nav>
-      </>
-    )
-  }
-}
+                <div>
+                  <p>{`${user.fname} ${user.lname}`}</p>
+                  <p>{user.headline}</p>
+                </div>
+              </div>
+              <div className='pf-btn' onClick={() => {
+                history.push(`/users/${user.id}`);
+                leave();
+              }}>View Profile</div>
+            </li>
+            <li onClick={logoutUser}>Sign Out</li>
+          </ul>
+        </button>
+      </nav>
+    </>
+  );
+};
 
 const mapSTP = ({ entities: { users }, session: { currentUser } }) => ({
   user: users[currentUser]
